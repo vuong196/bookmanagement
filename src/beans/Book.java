6473b429ed81342829ce3b9ee2c1d10a024fb16b
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-07-10 18:00:58 +0700

    BookManagement_BEANS_Book: Update Book entity after mapping with new added entities (Category and Author)

--- src/beans/Book.java
+++ src/beans/Book.java
@@ -1,24 +1,51 @@
 package beans;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity(name = "beans.Book")
+@Table(name = "book", catalog = "bookmanagement")
 public class Book {
 
 	private String _bookId;
+
 	private String _bookName;
-	private String _bookAuthor;
+
+	private Author _bookAuthor;
+
+	private Set<Category> _bookCategories = new HashSet<Category>(0);
 
 	public Book() {
 
 	}
 
-	public Book(String name, String author) {
+	public Book(String name, Set<Category> categories, Author author) {
 
-		this.set_bookId(UUID.randomUUID().toString());;
+		this.set_bookId(UUID.randomUUID().toString());
 		this.set_bookName(name);
+		this.set_bookCategories(categories);
 		this.set_bookAuthor(author);
 	}
 
+	@Id
+	@GeneratedValue(generator = "uuid")
+ 	@GenericGenerator(name = "uuid", strategy = "uuid2")
+  	@Column(name = "book_id", length = 36, unique = true, nullable = false)
 	public String get_bookId() {
 		return _bookId;
 	}
@@ -27,6 +54,7 @@ public class Book {
 		this._bookId = _bookId;
 	}
 
+	@Column(name = "book_name", length = 50, nullable = false)
 	public String get_bookName() {
 		return _bookName;
 	}
@@ -35,17 +63,26 @@ public class Book {
 		this._bookName = _bookName;
 	}
 
-	public String get_bookAuthor() {
+	@ManyToOne(fetch = FetchType.LAZY)
+  	@JoinColumn(name = "author_id", nullable = false)
+	public Author get_bookAuthor() {
 		return _bookAuthor;
 	}
 
-	public void set_bookAuthor(String _bookAuthor) {
+	public void set_bookAuthor(Author _bookAuthor) {
 		this._bookAuthor = _bookAuthor;
 	}
 
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "book_category",catalog = "bookmanagement", joinColumns = {
+		@JoinColumn(name = "book_id", nullable = false, updatable = false) },
+		inverseJoinColumns = { @JoinColumn(name = "category_id",
+			nullable = false, updatable = false) })
+	public Set<Category> get_bookCategories() {
+		return _bookCategories;
+	}
 
-
-
-
-
+	public void set_bookCategories(Set<Category> _bookCategories) {
+		this._bookCategories = _bookCategories;
+	}
 }
