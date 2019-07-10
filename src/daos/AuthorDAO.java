17e9b756530b144736cc633a1b363e21b580f2e3
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-07-10 17:55:48 +0700

    BookManagement_DAO_Author: Create AuthorDAO with CRUD services

---
+++ src/daos/AuthorDAO.java
@@ -0,0 +1,133 @@
+package daos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.query.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import beans.Author;
+import configurations.HibernateUtils;
+
+public class AuthorDAO {
+
+	 public static boolean save(String name) {
+
+		boolean status = true;
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+
+		try {
+			Author newAuthor = new Author(name);
+			session.save(newAuthor);
+			transaction.commit();
+		}
+		catch(Exception e)	{
+
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			session.getTransaction().rollback();
+			status = false;
+		}
+		session.close();
+
+		return status;
+	}
+
+	public static boolean update(String id, String name) {
+
+		boolean status = true;
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		Author Author = new Author();
+		Author.set_authorId(id);
+		Author.set_authorName(name);
+		try {
+			session.update(Author);
+			transaction.commit();
+		}
+		catch(Exception e)	{
+
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			session.getTransaction().rollback();
+			status = false;
+		}
+		session.close();
+
+		return status;
+	}
+
+	public static boolean delete(String id) {
+
+		boolean status = true;
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+
+			Author deleteAuthor = session.get(Author.class, id);
+			session.delete(deleteAuthor);
+			transaction.commit();
+		}
+		catch(Exception e)	{
+
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			session.getTransaction().rollback();
+			status = false;
+		}
+		session.close();
+
+		return status;
+	}
+
+	public static Author getAuthorById(String author_id) {
+
+		Author author = new Author();
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+
+			author = session.get(Author.class, author_id);
+			transaction.commit();
+		}
+		catch(Exception e)	{
+
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			session.getTransaction().rollback();
+		}
+		session.close();
+
+		return author;
+	}
+
+	public static List<Author> getAllAuthors() {
+		List<Author> authorRepository = new ArrayList<Author>();
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			Query<Author> query = session.createQuery("FROM " + Author.class.getName());
+			System.out.println(query);
+			authorRepository = (ArrayList<Author>) query.list();
+			transaction.commit();
+			} catch (Exception e) {
+
+				e.printStackTrace();
+				// Rollback trong trường hợp có lỗi xẩy ra.
+				transaction.rollback();
+			}
+
+		session.close();
+		return authorRepository;
+	}
+}
