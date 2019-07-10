c206e3a48426f5715006571cadd4e06a432704d5
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-07-10 17:50:06 +0700

    BookManagement_SERVLET_Book_EditForm: Rename EditFormServlet -> EditForm

--- src/servlets/book/EditForm.java
+++ src/servlets/book/EditForm.java
@@ -3,6 +3,7 @@ package servlets.book;
 import java.io.IOException;
 
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,10 +11,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.Author;
 import beans.Book;
+import beans.Category;
+import daos.AuthorDAO;
 import daos.BookDAO;
-@WebServlet("/EditFormServlet")
-public class EditFormServlet extends HttpServlet {
+import daos.CategoryDAO;
+@WebServlet("/EditBookFormServlet")
+public class EditForm extends HttpServlet {
 
 	private static final long _SERIAL_VERSION_UID = 1L;
 
@@ -36,6 +41,8 @@ public class EditFormServlet extends HttpServlet {
 		try {
 
 			Book book=BookDAO.getBookById(id);
+			List<Category> categories = CategoryDAO.getAllCategories();
+			List<Author> authors = AuthorDAO.getAllAuthors();
 
 			out.print("<form action='EditServlet' method='post'>");
 			out.print("<table>");
@@ -47,10 +54,17 @@ public class EditFormServlet extends HttpServlet {
 					+ "<td>Name:</td>"
 					+ "<td><input type='text' name='name' value='" + book.get_bookName() + "' required/></td>"
 					+ "</tr>");
-			out.print("<tr>"
-					+ "<td>Author:</td>"
-					+ "<td><input type='text' name='author' value='" + book.get_bookAuthor() + "'/>"
-					+ "</td></tr>");
+			out.print("<tr><td>Category:</td><td><select name='category'>");
+			for(Category c : categories) {
+				out.print("<option value ='"+ c.get_categoryId()+ "'>"+ c.get_categoryName()+ "</option>");
+			}
+			out.print("</select></td></tr>");
+
+			out.print("<tr><td>Author:</td><td><select name='author'>");
+			for(Author a : authors) {
+				out.print("<option value ='"+ a.get_authorId()+ "'>"+ a.get_authorName()+ "</option>");
+			}
+			out.print("</select></td></tr>");
 			out.print("<tr>"
 					+ "<td colspan='2'><input type='submit' value='Edit &amp; Save '/></td>"
 					+ "</tr>");
