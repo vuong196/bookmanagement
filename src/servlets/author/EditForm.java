7acd18be6b8d69c67b1d155a406401aa9b76a5cf
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-07-10 17:51:59 +0700

    BookManagement_SERVLET_Author: Create View, Add, AddForm, Edit, EditForm, Delete Servlets for Author

---
+++ src/servlets/author/EditForm.java
@@ -0,0 +1,63 @@
+package servlets.author;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Author;
+import daos.AuthorDAO;;
+@WebServlet("/EditAuthorFormServlet")
+public class EditForm extends HttpServlet {
+
+	private static final long _SERIAL_VERSION_UID = 1L;
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+
+		processRequest(request, response);
+
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Update Author</h1>");
+		String id = request.getParameter("id");
+
+		try {
+
+			Author a = AuthorDAO.getAuthorById(id);
+
+			out.print("<form action='EditServlet' method='post'>");
+			out.print("<table>");
+			out.print("<tr>"
+					+ "<td>Id:</td>"
+					+ "<td><input type='text' name='id' value='" + a.get_authorId() + "' readonly/></td>"
+					+ "</tr>");
+			out.print("<tr>"
+					+ "<td>Name:</td>"
+					+ "<td><input type='text' name='name' value='" + a.get_authorName() + "' required/></td>"
+					+ "</tr>");
+			out.print("<tr>"
+					+ "<td colspan='2'><input type='submit' value='Edit &amp; Save '/></td>"
+					+ "</tr>");
+			out.print("</table>");
+			out.print("</form>");
+		}
+		catch(Exception e) {
+
+			out.println("Caught error: " + e);
+		}
+
+		out.close();
+	}
+}
---
+++ src/servlets/author/View.java
@@ -0,0 +1,69 @@
+package servlets.author;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Author;
+import daos.AuthorDAO;
+@WebServlet("/ViewAuthorServlet")
+public class View extends HttpServlet {
+	private static final long _SERIAL_VERSION_UID = 1L;
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+
+		processRequest(request, response);
+		PrintWriter out = response.getWriter();
+		try {
+
+			List<Author> authors = AuthorDAO.getAllAuthors();
+			out.print("<center><h1> Author List </h1>");
+			out.print("<table border='1' cellpadding='4' width='60%'>"
+					+ "<tr><th>Id</th><th>Name</th><th>Features</th></tr>");
+			for(Author a : authors) {
+
+				out.print("<tr>"
+						+ "<td>"+a.get_authorId()+"</td>"
+						+ "<td>"+a.get_authorName()+"</td>"
+						+ "<td><center>"
+						+ "<a href='edit?id="+a.get_authorId()+"'>Edit</a>  "
+						+ "<a href='delete?id="+a.get_authorId()+"'>Delete</a>"
+						+ "</center></td>"
+						+ "</tr>");
+			}
+			out.print("</table></center><br/>");
+			out.println("<center><a href='add'> Add New Author </a></center>");
+			out.print("<a href='../index.html'>Homepage</a>");
+			out.close();
+		}
+		catch (Exception e) {
+
+			out.println("Caught error: " + e);
+		}
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+
+		processRequest(request, response);
+	}
+}
+
+
+
