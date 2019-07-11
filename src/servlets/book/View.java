763f534b89e5b068bb8adebc75a077e6a709bdae
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-07-11 16:28:38 +0700

    BookManagement_SERVLET_Book_View: Add column "categories" to the table displaying book list

--- src/servlets/book/View.java
+++ src/servlets/book/View.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import beans.Book;
+import beans.Category;
 import daos.AuthorDAO;
 import daos.BookDAO;
 @WebServlet("/ViewBookServlet")
@@ -34,13 +35,17 @@ public class View extends HttpServlet {
 			List<Book> books = BookDAO.getAllBooks();
 			out.print("<center><h1> Book List </h1>");
 			out.print("<table border='1' cellpadding='4' width='60%'>"
-					+ "<tr><th>Name</th><th>Author</th><th>Features</th></tr>");
+					+ "<tr><th>Name</th><th>Author</th><th>Categories</th><th>Actions</th></tr>");
 			for(Book b:books) {
 				System.out.println(b.get_bookAuthor().get_authorName());
 				out.print("<tr>"
 						+ "<td>"+b.get_bookName()+"</td>"
 						+ "<td>"+b.get_bookAuthor().get_authorName()+"</td>"
-						+ "<td><center>"
+						+ "<td><center>");
+				for (Category c: b.get_bookCategories()) {
+					out.print("<label>"+c.get_categoryName()+"</label>");
+				};
+				out.print("<td><center>"
 						+ "<a href='edit?id="+b.get_bookId()+"'>Edit</a>  "
 						+ "<a href='delete?id="+b.get_bookId()+"'>Delete</a>"
 						+ "</center></td>"
