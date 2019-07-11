3ae5e79bf054667cc31e3f05e8c9a2ebcf4ee0fe
Vuong Nguyen <vuong.nguyen@codeenginestudio.com>
2019-07-11 16:36:07 +0700

    BookManagement_DAO: Format code in Author, Book, Category DAOs

--- src/daos/BookDAO.java
+++ src/daos/BookDAO.java
@@ -1,3 +1,4 @@
+
 package daos;
 
 import java.util.ArrayList;
@@ -5,80 +6,17 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.hibernate.query.Query;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
+import beans.Author;
 import beans.Book;
 import beans.Category;
-import beans.Author;
 import configurations.HibernateUtils;
 
 public class BookDAO {
-
-	 public static boolean save(String name, String category_id, String author_id) {
-
-		boolean status = true;
-		SessionFactory factory = HibernateUtils.getSessionFactory();
-		Session session = factory.getCurrentSession();
-		Transaction transaction = session.beginTransaction();
-
-		try {
-			Author author = session.get(Author.class, author_id);
-			Set<Category> categories = new HashSet<Category>();
-			categories.add(session.get(Category.class, category_id));
-
-			Book newBook = new Book(name, categories, author);
-			session.save(newBook);
-			transaction.commit();
-		}
-		catch(Exception e)	{
-
-			e.printStackTrace();
-			// Rollback trong trường hợp có lỗi xẩy ra.
-			session.getTransaction().rollback();
-			status = false;
-		}
-		session.close();
-
-		return status;
-	}
-
-	public static boolean update(String id, String name, String category_id, String author_id) {
-
-		boolean status = true;
-		SessionFactory factory = HibernateUtils.getSessionFactory();
-		Session session = factory.getCurrentSession();
-		Transaction transaction = session.beginTransaction();
-
-		try {
-
-			Author author = session.get(Author.class, author_id);
-			Set<Category> categories = new HashSet<Category>();
-			categories.add(session.get(Category.class, category_id));
-
-			Book book = session.get(Book.class, id);
-			book.set_bookName(name);
-			book.set_bookAuthor(author);
-			book.set_bookCategories(categories);
-
-			session.update(book);
-			transaction.commit();
-		}
-		catch(Exception e)	{
-
-			e.printStackTrace();
-			// Rollback trong trường hợp có lỗi xẩy ra.
-			session.getTransaction().rollback();
-			status = false;
-		}
-		session.close();
-
-		return status;
-	}
 
 	public static boolean delete(String id) {
 
@@ -87,21 +25,37 @@ public class BookDAO {
 		Session session = factory.getCurrentSession();
 		Transaction transaction = session.beginTransaction();
 		try {
-
 			Book deleteBook = session.get(Book.class, id);
 			session.delete(deleteBook);
 			transaction.commit();
-		}
-		catch(Exception e)	{
-
+		} catch (Exception e) {
 			e.printStackTrace();
 			// Rollback trong trường hợp có lỗi xẩy ra.
 			session.getTransaction().rollback();
 			status = false;
 		}
 		session.close();
-
 		return status;
+	}
+
+	public static List<Book> getAllBooks() {
+
+		List<Book> bookRepository = new ArrayList<>();
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			Query<Book> query = session.createQuery("FROM " + Book.class.getName());
+			System.out.println(query);
+			bookRepository = query.list();
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			transaction.rollback();
+		}
+		session.close();
+		return bookRepository;
 	}
 
 	public static Book getBookById(String id) {
@@ -111,40 +65,63 @@ public class BookDAO {
 		Session session = factory.getCurrentSession();
 		Transaction transaction = session.beginTransaction();
 		try {
-
 			book = session.get(Book.class, id);
 			transaction.commit();
-		}
-		catch(Exception e)	{
-
+		} catch (Exception e) {
 			e.printStackTrace();
 			// Rollback trong trường hợp có lỗi xẩy ra.
 			session.getTransaction().rollback();
 		}
 		session.close();
-
 		return book;
 	}
 
-	public static List<Book> getAllBooks() {
-		List<Book> bookRepository = new ArrayList<Book>();
+	public static boolean save(String name, String category_id, String author_id) {
+
+		boolean status = true;
 		SessionFactory factory = HibernateUtils.getSessionFactory();
 		Session session = factory.getCurrentSession();
 		Transaction transaction = session.beginTransaction();
 		try {
-			Query<Book> query = session.createQuery("FROM " + Book.class.getName());
-			System.out.println(query);
-			bookRepository = (ArrayList<Book>) query.list();
+			Author author = session.get(Author.class, author_id);
+			Set<Category> categories = new HashSet<>();
+			categories.add(session.get(Category.class, category_id));
+			Book newBook = new Book(name, categories, author);
+			session.save(newBook);
 			transaction.commit();
-			} catch (Exception e) {
-
-				e.printStackTrace();
-				// Rollback trong trường hợp có lỗi xẩy ra.
-				transaction.rollback();
-			}
-
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			session.getTransaction().rollback();
+			status = false;
+		}
 		session.close();
-		return bookRepository;
+		return status;
 	}
 
+	public static boolean update(String id, String name, String category_id, String author_id) {
+
+		boolean status = true;
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			Author author = session.get(Author.class, author_id);
+			Set<Category> categories = new HashSet<>();
+			categories.add(session.get(Category.class, category_id));
+			Book book = session.get(Book.class, id);
+			book.set_bookName(name);
+			book.set_bookAuthor(author);
+			book.set_bookCategories(categories);
+			session.update(book);
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			session.getTransaction().rollback();
+			status = false;
+		}
+		session.close();
+		return status;
+	}
 }
--- src/daos/CategoryDAO.java
+++ src/daos/CategoryDAO.java
@@ -1,42 +1,95 @@
+
 package daos;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.query.Query;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import beans.Category;
 import configurations.HibernateUtils;
 
 public class CategoryDAO {
 
-	 public static boolean save(String name) {
+	public static boolean delete(String id) {
 
 		boolean status = true;
 		SessionFactory factory = HibernateUtils.getSessionFactory();
 		Session session = factory.getCurrentSession();
 		Transaction transaction = session.beginTransaction();
-
 		try {
-			Category newCategory = new Category(name);
-			System.out.println(newCategory);
-			session.save(newCategory);
+			Category deleteCategory = session.get(Category.class, id);
+			session.delete(deleteCategory);
 			transaction.commit();
-		}
-		catch(Exception e)	{
-
+		} catch (Exception e) {
 			e.printStackTrace();
 			// Rollback trong trường hợp có lỗi xẩy ra.
 			session.getTransaction().rollback();
 			status = false;
 		}
 		session.close();
+		return status;
+	}
 
+	public static List<Category> getAllCategories() {
+
+		List<Category> categoryRepository = new ArrayList<>();
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			Query<Category> query = session.createQuery("FROM " + Category.class.getName());
+			System.out.println(query);
+			categoryRepository = query.list();
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			transaction.rollback();
+		}
+		session.close();
+		return categoryRepository;
+	}
+
+	public static Category getCategoryById(String id) {
+
+		Category category = new Category();
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			category = session.get(Category.class, id);
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			session.getTransaction().rollback();
+		}
+		session.close();
+		return category;
+	}
+
+	public static boolean save(String name) {
+
+		boolean status = true;
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			Category newCategory = new Category(name);
+			System.out.println(newCategory);
+			session.save(newCategory);
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Rollback trong trường hợp có lỗi xẩy ra.
+			session.getTransaction().rollback();
+			status = false;
+		}
+		session.close();
 		return status;
 	}
 
@@ -52,84 +105,13 @@ public class CategoryDAO {
 		try {
 			session.update(category);
 			transaction.commit();
-		}
-		catch(Exception e)	{
-
+		} catch (Exception e) {
 			e.printStackTrace();
 			// Rollback trong trường hợp có lỗi xẩy ra.
 			session.getTransaction().rollback();
 			status = false;
 		}
 		session.close();
-
 		return status;
-	}
-
-	public static boolean delete(String id) {
-
-		boolean status = true;
-		SessionFactory factory = HibernateUtils.getSessionFactory();
-		Session session = factory.getCurrentSession();
-		Transaction transaction = session.beginTransaction();
-		try {
-
-			Category deleteCategory = session.get(Category.class, id);
-			session.delete(deleteCategory);
-			transaction.commit();
-		}
-		catch(Exception e)	{
-
-			e.printStackTrace();
-			// Rollback trong trường hợp có lỗi xẩy ra.
-			session.getTransaction().rollback();
-			status = false;
-		}
-		session.close();
-
-		return status;
-	}
-
-	public static Category getCategoryById(String id) {
-
-		Category category = new Category();
-		SessionFactory factory = HibernateUtils.getSessionFactory();
-		Session session = factory.getCurrentSession();
-		Transaction transaction = session.beginTransaction();
-		try {
-
-			category = session.get(Category.class, id);
-			transaction.commit();
-		}
-		catch(Exception e)	{
-
-			e.printStackTrace();
-			// Rollback trong trường hợp có lỗi xẩy ra.
-			session.getTransaction().rollback();
-		}
-		session.close();
-
-		return category;
-	}
-
-	public static List<Category> getAllCategories() {
-		List<Category> categoryRepository = new ArrayList<Category>();
-		SessionFactory factory = HibernateUtils.getSessionFactory();
-		Session session = factory.getCurrentSession();
-		Transaction transaction = session.beginTransaction();
-		try {
-			Query<Category> query = session.createQuery("FROM " +  Category.class.getName());
-			System.out.println(query);
-			categoryRepository = (ArrayList<Category>) query.list();
-			transaction.commit();
-		}
-		catch (Exception e) {
-
-				e.printStackTrace();
-				// Rollback trong trường hợp có lỗi xẩy ra.
-				transaction.rollback();
-			}
-
-		session.close();
-		return categoryRepository;
 	}
 }
